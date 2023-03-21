0d4423937315d23bd8f0379d5d2c51adab3751e2
Topala Ion <apostion30@gmail.com>
2023-03-21 21:34:19 +0200

    beta blocker

--- test.py
+++ test.py
@@ -16,40 +16,38 @@ class MedicineApp:
         master.title("Medicine App")
 
         # Medication name input
-        self.med_label = tk.Label(master, text="Enter medication name:", font=('Arial 14'))
+        self.med_label = tk.Label(master, text="Enter medication name:", font=('Arial 12'))
         self.med_label.grid(row=0, column=0, padx=(75, 20), pady=20)
 
-        self.med_entry = tk.Entry(master, font=('Arial 14'))
+        self.med_entry = tk.Entry(master, font=('Arial 12'))
         self.med_entry.grid(row=0, column=1, padx=(75, 20), pady=10)
 
         # Predefined questions
         prologQueries = {
             1: "Show drug's medication class ",
-            2: "Query drug's side effects"
+            2: "Query drug's side effects",
+            3: "What medicine interacts negative?",
+            4: "Is the drug a beta-blocker?",
+            5: "For what disorders is contraindicated for?",
+            6: "Is it the drug safe for pregnancy?"
         }
-        # self.questions = [
-        #     "What is the dosage for this medication?",
-        #     "What are the potential side effects of this medication?",
-        #     "How long should I take this medication for?",
-        #     "What should I do if I miss a dose of this medication?"
-        # ]
 
         # Radio buttons for selecting questions
         self.selected_question = tk.IntVar()
         self.radio_buttons = []
         for i, question in enumerate(prologQueries):
             radio_button = tk.Radiobutton(master, text=(prologQueries[question]), variable=self.selected_question,
-                                          font=('Arial 14'),
+                                          font=('Arial 10'),
                                           value=question)
             radio_button.grid(row=i + 1, column=0, padx=(75, 20), pady=10, sticky=tk.W)
             self.radio_buttons.append(radio_button)
 
         # Button to show answer for selected question
-        self.answer_button = tk.Button(master, text="Show answer", font=('Arial 14'), command=self.show_answer)
+        self.answer_button = tk.Button(master, text="Show answer", font=('Arial 10'), command=self.show_answer)
         self.answer_button.grid(row=7, column=0, padx=(75, 20), pady=10, sticky=tk.W)
 
         # Button to clear answer label
-        self.clear_button = tk.Button(master, text="Clear answer", font=('Arial 14'), command=self.clear_answer)
+        self.clear_button = tk.Button(master, text="Clear answer", font=('Arial 10'), command=self.clear_answer)
         self.clear_button.grid(row=7, column=1, padx=(75, 20), pady=10, sticky=tk.W)
 
         # Button to close the program
@@ -63,7 +61,7 @@ class MedicineApp:
 
     def show_answer(self):
         # Get current medication name
-        medication = self.med_entry.get()
+        medication = str(self.med_entry.get()).lower()
 
         # Get selected question
         key = self.selected_question.get()
@@ -72,6 +70,10 @@ class MedicineApp:
             self.query_medication_class(medication)
         elif key == 2:
             self.query_side_effect(medication)
+        elif key == 3:
+            self.interacts_negative(medication)
+        elif key == 4:
+            self.is_beta_blocker(medication)
 
         # Display question
         # self.question_label.config(text=question)
@@ -95,6 +97,18 @@ class MedicineApp:
         for result in prolog.query(f'side_effect({input_text}, SideEffect)'):
             self.results.insert(END,
                                 f'{input_text.title()}\'s side effect is {str(result["SideEffect"]).title()}' + '\n')
+
+    def interacts_negative(self, input_text):
+        for result in prolog.query(f'interacts_with({input_text}, Medicine)'):
+            self.results.insert(END,
+                                f'{input_text.title()} interacts negative with {str(result["Medicine"]).title()}' + '\n')
+
+    def is_beta_blocker(self, input_text):
+        for result in prolog.query(f'is_beta_blocker({input_text})'):
+            if len(result) == 0:
+                self.results.insert(END, 'Medication is a beta blocker' + '\n')
+                return
+        self.results.insert(END, 'Medication is not a beta blocker' + '\n')
 
     def clear_answer(self):
         # Clear answer label
